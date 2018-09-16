348c82ced93d91056f1d5cc704a576521a80fc57
YunchaoLiu <yunchao.liu@vanderbilt.edu>
2018-09-16 18:24:25 -0500

    DONE:fix a bug in text expanding. TODO: text collapsing

--- assignment2/interactive_time_series.js
+++ assignment2/interactive_time_series.js
@@ -48,7 +48,7 @@ function create_color(root_node)  {
 }
 
 // TODO: create a time series for each non-leaf node (`counts`) that aggregates its count data and dates - same format as `counts` in leaves
-var is_mean = false;//Change this flag to toggle between mean and sum;True for sum, False for mean.
+var is_mean = true;//Change this flag to toggle between mean and sum;True for sum, False for mean.
 function aggregate_counts(node)  {
 	//console.log("node_name:" +node.name);
 	
@@ -288,7 +288,12 @@ function visualize_time_series(root_node, is_collapsing, selected_node)  {
 
 	if(is_collapsing == false){
 		if(selected_node != undefined)
-			var text_s = d3.selectAll('#'+selected_node.name).selectAll('text').data(node_array);
+		{
+			for(var i = 0; i<selected_node.children.length;i++){	
+				
+				var text_s = d3.selectAll('#'+selected_node.children[i].name).selectAll('text').data(node_array);
+			}
+		}
 		else
 			var text_s = d3.selectAll('#'+root_node.name).selectAll('text').data(node_array)
 
