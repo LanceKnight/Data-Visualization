b690a9fb081bc7a66451f3186a319bca137ba51e
Matthew Berger <matthew.berger@vanderbilt.edu>
2018-10-15 07:29:36 -0500

    Assignment 5

---
+++ assignment5/treemap.js
@@ -0,0 +1,35 @@
+var global_cushion_scale = 0.0; 
+
+// compute depths, concatenate package names, leaf determination, accumulate sizes...
+function preprocess_tree(node, concat_names, depth)  {
+	node.full_name = depth==0 ? node.name : concat_names+'.'+node.name;
+	node.depth = depth+1;
+	if(node.depth == 1)
+		node.box = {ll:[0,0], ur:[1,1]};
+
+	if('children' in node)  {
+		node.is_leaf = false;
+		node.value = 0;
+		for(var c = 0; c < node.children.length; c++)  {
+			node.children[c].parent = node;
+			preprocess_tree(node.children[c], node.full_name, node.depth);
+		}
+	}
+	else  {
+		node.is_leaf = true;
+		node.value = +node.value;
+		node.children = [];
+	}
+}
+
+function plot_it()  {
+	// preprocess the tree
+	preprocess_tree(flare_data, '', 0);
+
+	// setup svg element
+	var width = 750, height = 750;
+	var pad = 80;
+	d3.select('body').append('svg').attr('width',width).attr('height',height);
+
+	// do treemap!
+}
